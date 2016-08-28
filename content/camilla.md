40a8ddc33367edb4a877efeeb7604d84ebce7444
Pietro Bertera <pietro@bertera.it>
2016-08-28 16:17:14 +0200

    Added some scripts

--- content/camilla.md
+++ content/camilla.md
@@ -6,9 +6,9 @@ Tags: booking, php
 Slug: camilla
 Status: published
 
-Camilla Ã¨ un sistema di booking per camere e case, supporta agenzie
+Camilla è un sistema di booking per camere e case, supporta agenzie
 multiple e la gestione di calendari e stagioni.  
-![](http://www.bertera.it/software/camilla/camilla.jpg)  
+![camilla]({attach}/static/camilla.jpg)  
 
 [camilla-0.9.tgz](http://www.bertera.it/software/camilla/camilla-0.9.tgz)
 
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONTAINER=pbertera/pelican
+LOCAL_PORT=8000
+SSH_KEY=.ssh/id_dsa
+
+case $1 in
+    deploy)
+        docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER github
+        ;;
+    serve)
+        docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER serve 
+        ;;
+    *)
+        docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER $1
+        ;; 
+esac
