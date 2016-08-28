2f64d0769ff92e5a10800153929b0337d2d0f35d
Pietro Bertera <pietro@bertera.it>
2016-08-28 22:54:57 +0200

    MOD: modified the manage.sh script

--- manage.sh
+++ manage.sh
@@ -8,11 +8,12 @@ case $1 in
     deploy)
         docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER github
         ;;
-    serve)
-        docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER serve 
+    update)
+        docker run -it -v ${PWD}:/site $CONTAINER html && docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} $CONTAINER serve 
         ;;
     build)
         docker build -t ${CONTAINER} .
+        ;;
     *)
         docker run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER $1
         ;; 
