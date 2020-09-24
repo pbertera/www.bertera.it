508914fe4c40c3752c2381cf41ad1725b7369f26
Pietro Bertera <pietro@bertera.it>
2020-09-24 11:19:09 +0200

    rootless podman

--- manage.sh
+++ manage.sh
@@ -2,11 +2,11 @@
 
 CONTAINER=pbertera/pelican
 LOCAL_PORT=8000
-SSH_KEY=.ssh/id_dsa
+SSH_KEY=.ssh/id_rsa
 
 case $1 in
     deploy)
-        podman run -it -v ${PWD}:/site -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY} $CONTAINER github
+        podman run -it -v ${PWD}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} -v ${HOME}/${SSH_KEY}:/root/${SSH_KEY}:Z $CONTAINER github
         ;;
     update)
         podman run -it -v ${PWD}:/site:rw,Z $CONTAINER html && podman run -it -v ${PWD}:/site:rw,Z -p ${LOCAL_PORT}:${LOCAL_PORT} $CONTAINER serve 
